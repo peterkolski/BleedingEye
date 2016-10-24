d000da18480abc84211cc96456a3f5c8c891c33a
Peter Kolski <peter@bildpeter.de>
2016-10-25 00:19:15 +0200

    namespace

--- shared/Network.cpp
+++ shared/Network.cpp
@@ -4,6 +4,9 @@
 
 #include "Network.h"
 
+namespace bildpeter
+{
+
 void Network::networkSetup( int numPoints )
 {
     network.setCorrelation( 777 );
@@ -12,67 +15,66 @@ void Network::networkSetup( int numPoints )
 
 }
 
-void    Network::setRandomPositions( int _num )
+void Network::setRandomPositions( int _num )
 {
-    ofLogVerbose()  << "Setting random node position";
-    network.nodes_.clear();
-    for ( int i = 0; i  < _num; ++i)
-    {
+    ofLogVerbose( ) << "Setting random node position";
+    network.nodes_.clear( );
+    for ( int i = 0; i < _num; ++i ) {
         network.nodes_.emplace_back( i );
-        network.nodes_.back().setRandomPosition( -(ofGetWidth() / 2), ofGetWidth() / 2,
-                                                -(ofGetWidth() / 2), ofGetWidth() / 2,
-                                                -(ofGetWidth() / 2), ofGetWidth() / 2 );
+        network.nodes_.back( ).setRandomPosition( -( ofGetWidth( ) / 2 ), ofGetWidth( ) / 2,
+                                                  -( ofGetWidth( ) / 2 ), ofGetWidth( ) / 2,
+                                                  -( ofGetWidth( ) / 2 ), ofGetWidth( ) / 2 );
 
-        ofLogVerbose()  << "node ID: " << network.nodes_.back().getId() << " ( "
-                        << network.nodes_.back().getPosition().x << ", "
-                        << network.nodes_.back().getPosition().y << ", "
-                        << network.nodes_.back().getPosition().z << " )";
+        ofLogVerbose( ) << "node ID: " << network.nodes_.back( ).getId( ) << " ( "
+                        << network.nodes_.back( ).getPosition( ).x << ", "
+                        << network.nodes_.back( ).getPosition( ).y << ", "
+                        << network.nodes_.back( ).getPosition( ).z << " )";
     }
 }
 
 void Network::networkUpdate( float fade, float movement, float distCenter, float distDiff, float arm, float back,
                              float shoulder, float movementSensor, float distCenterSensor, float distDiffSensor )
 {
-    netMoveSensor       = ( 1 - arm ) * movementSensor;
-    netDiffSensor       = ( 1 - back ) * distDiffSensor;
-    netCenterSensor     = ( 1 - shoulder ) * distCenterSensor;
+    netMoveSensor = ( 1 - arm ) * movementSensor;
+    netDiffSensor = ( 1 - back ) * distDiffSensor;
+    netCenterSensor = ( 1 - shoulder ) * distCenterSensor;
 
-    if ( fade )
-    {
+    if ( fade ) {
         // TODO get rid of copying
-        auto    opacityMain         = fade;
+        auto opacityMain = fade;
 //        auto    opacityFromSensor   = midiUC.getValue("networkFadeSensor" ) * oscData[ 2 ];
-        auto    movementMain        = movement;
+        auto movementMain = movement;
 //        auto    movementFromSensor  = midiUC.getValue("networkMovementSensor") * oscData[ 0 ];
-        auto    distanceCenterMain  = distCenter;
+        auto distanceCenterMain = distCenter;
 //        auto    distanceCenterSensor = midiUC.getValue("networkDistCenterSensor") * oscData[ 4 ];
-        auto    distanceDiffMain    = distDiff;
+        auto distanceDiffMain = distDiff;
 //        auto    distanceDiffSensor  = midiUC.getValue("networkDistDiffSensor") * oscData[ 6 ];
 
-        distanceCenter      = ofClamp( distanceCenterMain   - netCenterSensor, 0.0, 1.0 ) * distanceMaxCenter;
-        distanceDifference  = ofClamp( distanceDiffMain     - netDiffSensor, 0.0, 1.0 ) * distanceMaxDifference;
-        minDist             = distanceCenter - distanceDifference / 2;
-        maxDist             = distanceCenter + distanceDifference / 2;
-        valOpacity          = ofClamp( opacityMain, 0.0, 1.0);
-        valMovement         = ofClamp( movementMain - netMoveSensor, 0.0, 1.0 ) * movementMax;
+        distanceCenter = ofClamp( distanceCenterMain - netCenterSensor, 0.0, 1.0 ) * distanceMaxCenter;
+        distanceDifference = ofClamp( distanceDiffMain - netDiffSensor, 0.0, 1.0 ) * distanceMaxDifference;
+        minDist = distanceCenter - distanceDifference / 2;
+        maxDist = distanceCenter + distanceDifference / 2;
+        valOpacity = ofClamp( opacityMain, 0.0, 1.0 );
+        valMovement = ofClamp( movementMain - netMoveSensor, 0.0, 1.0 ) * movementMax;
 
         // --- NETWORK
         network.setMaxMovement( valMovement );
-        network.update();
+        network.update( );
     }
 }
 
 void Network::networkDraw( float fade )
 {
-    if ( fade )
-    {
-        if ( isPointDrawing )   network.drawPoints( 2, ofColor_::gray );
+    if ( fade ) {
+        if ( isPointDrawing ) network.drawPoints( 2, ofColor_::gray );
 
-        ofPushMatrix();
+        ofPushMatrix( );
         {
-            ofTranslate( ofGetWidth() / 2, ofGetHeight() / 2, -1000 );
-            network.draw( minDist, maxDist,  valOpacity );
+            ofTranslate( ofGetWidth( ) / 2, ofGetHeight( ) / 2, -1000 );
+            network.draw( minDist, maxDist, valOpacity );
         }
-        ofPopMatrix();
+        ofPopMatrix( );
     }
+}
+
 }
--- shared/Network.h
+++ shared/Network.h
@@ -7,36 +7,37 @@
 
 #include "NetworkVisual.hpp"
 
-class Network {
+namespace bildpeter
+{
 
+class Network
+{
 public:
-    // --- Network
     void networkSetup( int numPoints );
 
-    bildpeter::NetworkVisual    network;
-    // --- VALUES
-    float           valMovement;
-    float           valOpacity;
-    float           movementMax             = 500.0;
-    float           distanceMaxDifference   = 500.0;
-    float           distanceMaxCenter       = 500.0;
-    float           distanceDifference;
-    float           distanceCenter;
-    float minDist;
-    float maxDist;
-    float netMoveSensor;
-    float netConnectionSensor;
-    float netDiffSensor;
-    float netCenterSensor;
-    bool                isPointDrawing  = false;
-
-    void                setRandomPositions( int _num );
+    void setRandomPositions( int _num );
 
     void networkUpdate( float fade, float movement, float distCenter, float distDiff, float arm, float back,
                         float shoulder, float movementSensor, float distCenterSensor, float distDiffSensor );
 
     void networkDraw( float fade );
-};
 
+private:
+    bildpeter::NetworkVisual network;
+    float valMovement;
+    float valOpacity;
+    float movementMax = 500.0;
+    float distanceMaxDifference = 500.0;
+    float distanceMaxCenter = 500.0;
+    float distanceDifference;
+    float distanceCenter;
+    float minDist;
+    float maxDist;
+    float netMoveSensor;
+    float netDiffSensor;
+    float netCenterSensor;
+    bool isPointDrawing = false;
+};
+}
 
 #endif //BLEEDINGEYE_NETWORK_H
