1698ccf4ea9ff005c3f800006c95336e9e78ff73
Peter Kolski <peter@bildpeter.de>
2016-11-29 17:06:21 +0100

    Own VideoSwitch Class

---
+++ shared/VideoSwitch.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Peter A. Kolski on 29.11.16.
+//
+
+#include "VideoSwitch.h"
+
+bool bildpeter::VideoSwitch::grabVideoKey( int key )
+{
+
+    return false;
+}
+
+bool bildpeter::VideoSwitch::grabBankKey( int key )
+{
+
+    return false;
+}
---
+++ shared/VideoSwitch.h
@@ -0,0 +1,26 @@
+//
+// Created by Peter A. Kolski on 29.11.16.
+//
+
+#ifndef BLEEDINGEYE_VIDEOSWITCH_H
+#define BLEEDINGEYE_VIDEOSWITCH_H
+
+#include <map>
+#include <vector>
+
+namespace bildpeter
+{
+class VideoSwitch
+{
+public:
+    bool grabVideoKey( int key );
+    bool grabBankKey( int key );
+
+private:
+    std::map< int, int > videoMap = { {1,1}, {2,2}, {3,3}, {4,4}, {5,5}, {6,6}, {7,7}, {8,8}, {9,9}, {0,0} };
+//    std::vector< int > bankMap  = { 'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O' };
+};
+} // namespace bildpeter
+
+
+#endif //BLEEDINGEYE_VIDEOSWITCH_H
