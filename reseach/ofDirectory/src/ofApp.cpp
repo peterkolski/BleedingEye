27817aa0baef400d9e03ee1111503934dfdae58e
Peter Kolski <peter@bildpeter.de>
2016-12-01 19:15:05 +0100

    output, if there is a directory

--- reseach/ofDirectory/src/ofApp.cpp
+++ reseach/ofDirectory/src/ofApp.cpp
@@ -3,6 +3,16 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    dirList_.listDir( "/Users/sonneundasche/programming/of/apps/BleedingEye/reseach/ofDirectory/folders/A" );
+
+    for( auto &file : dirList_.getFiles() )
+    {
+        ofLogNotice() << file.path()
+                      << " | F: " << file.isFile()
+                      << " | D: " << file.isDirectory()
+                      << "\n";
+    }
+
 }
 
 //--------------------------------------------------------------
