d31fba91d763027e23347e87c5362cef54fbfd4c
Peter Kolski <peter@bildpeter.de>
2016-12-01 18:50:47 +0100

    Research of ofDirectory

---
+++ reseach/ofDirectory/src/ofApp.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ofMain.h"
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+/// For midi we need multiple inheritance
+class ofApp : public ofBaseApp
+{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+};
