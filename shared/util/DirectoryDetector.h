d6aee62d6ad0e2196cde862863070d37abd0b9fe
Peter Kolski <peter@bildpeter.de>
2016-12-01 19:26:00 +0100

    Basic var & methods

--- shared/util/DirectoryDetector.h
+++ shared/util/DirectoryDetector.h
@@ -5,11 +5,21 @@
 #ifndef BLEEDINGEYE_DIRECTORYDETECTOR_H
 #define BLEEDINGEYE_DIRECTORYDETECTOR_H
 
+#include "ofMain.h"
+
 namespace bildpeter
 {
 class DirectoryDetector
 {
+public:
+    void    readDirectories( string path );
 
+    int                     getPathsAmount()    { return paths_.size(); }
+    std::vector< string >   getPaths()          { return paths_; }
+
+private:
+    ofDirectory             dirList_;
+    std::vector< string >   paths_;
 };
 } // namespace bildpeter
 
